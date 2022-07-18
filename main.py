fed39b9a08d16dcb78e23e8aa443355313d2ca1b
roysmusic <107501496+roysmusic@users.noreply.github.com>
2022-07-18 04:26:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import asyncio
 from pytgcalls import idle
 from driver.veez import call_py, bot, user
 
+import requests
+from pyrogram import idle
+from pyrogram import Client as Bot
 
-async def start_bot():
-    await bot.start()
-    print("[INFO]: BOT & UBOT CLIENT STARTED !!")
-    await call_py.start()
-    print("[INFO]: PY-TGCALLS CLIENT STARTED !!")
-    await user.join_chat("sohbeti_muhabbet")
-    await user.join_chat("graiflyrics")
-    await idle()
-    print("[INFO]: STOPPING BOT & USERBOT")
-    await bot.stop()
+from callsmusic import run
+from config import API_ID, API_HASH, BOT_TOKEN
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_bot())
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="handlers")
+)
+
+bot.start()
+run()
+idle()
